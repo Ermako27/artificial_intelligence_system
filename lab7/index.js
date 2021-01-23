afe541e924f8df32466c3f442483b7a774bb5983
Ermako27 <ermakov.1828@gmail.com>
2021-01-23 15:44:56 +0300

    lab7: done query for certain param

--- lab7/index.js
+++ lab7/index.js
@@ -11,9 +11,9 @@ async function handleRequest(req, res) {
     const intention = await detectIntent(query, client, path);
 
     // что хотел сделать пользователь и какая модель акустики
-    const {queryResult: {parameters: { fields: {model: {stringValue} } }, intent: {displayName}}} =  intention[0];
+    // const {queryResult: {parameters: { fields: {model: {stringValue} } }, intent: {displayName}}} =  intention[0];
 
-    const response = processQuery(displayName, stringValue);
+    const response = processQuery(intention[0]);
 
     res.status(200).json(response);
     
--- lab7/queryProcessor.js
+++ lab7/queryProcessor.js
@@ -9,11 +9,51 @@ const paramsMap = {
     'acoustic_design': 'акустическое оформление'
 };
 
-function processQuery(intent, model) {
-    switch (intent) {
-        case 'get_all_params':
-            return buildAllParamsResponse(model)
+const reverseParamMap = {
+    'максимальная мощность': 'max_power',
+    'максимальная частота': 'max_frequency',
+    'минимальная частота': 'min_frequency',
+    'высота корпуса': 'body_height',
+    'беспроводные': 'wireless',
+    'акустическое оформление': 'acoustic_design'
+}
+
+function processQuery(intention) {
+
+    const { queryResult: { intent: { displayName: userIntent } } } = intention;
+
+    let response;
+
+    if (userIntent === 'get_all_params') {
+        const { queryResult: {
+                    parameters: {
+                        fields: {
+                            model: { 
+                                stringValue: modelName
+                            } 
+                        } 
+                    } 
+                } 
+            } =  intention;
+        response = buildAllParamsResponse(modelName);
+    } else if (userIntent === 'get_certain_param') {
+        const { queryResult: {
+                    parameters: {
+                        fields: {
+                            model: { 
+                                stringValue: modelName 
+                            },
+                            param: {
+                                stringValue: param 
+                            }
+                        }
+                    } 
+                } 
+            } = intention;
+        response = buildCertainParamResponse(modelName, param);
     }
+
+    return response
 }
 
 function getModelByName(name) {
@@ -33,6 +73,21 @@ function buildAllParamsResponse(modelName) {
     }
 
     return response
+}
+
+/**
+ * 
+ * @param {*} modelName название модели
+ * @param {*} param рукописный параметр, который ввел пользователь, напирмер "акустическое оформление"
+ */
+function buildCertainParamResponse(modelName, param) {
+    const { params } = getModelByName(modelName);
+
+    const paramKey = reverseParamMap[param];
+
+    return paramKey === 'wireless' ?
+            `Модель ${modelName}, ${param}: ${params[paramKey] ? 'да' : 'нет'}` :
+            `Модель ${modelName}, ${param}: ${params[paramKey]}`;
 }
 
 module.exports = {
